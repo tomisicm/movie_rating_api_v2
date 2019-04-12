86dda2d057bd917ae5b3c5fdb2265dd894be3a69
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-04-12 22:54:48 +0200

    i am a pleb

--- src/resources/user/user.controller.js
+++ src/resources/user/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from './user.model'
 import multer from 'multer'
+import _ from 'lodash'
 
 /* deviation from standard REST API structure */
 /* user can only get his info and update his info */
@@ -31,13 +32,29 @@ export const updateMe = async (req, res) => {
 }
 
 export const getMany = async (req, res) => {
+  let rq = req.query
   const { perPage, page } = req.query
+
+  let query = null
+  if (!_.isEmpty(rq)) {
+    query = {
+      $or: [
+        // eslint-disable-next-line prettier/prettier
+        { 'name': { $regex: rq.query, $options: 'i' } }
+        // this will be expaned later once i add user profile section
+        // where the plebs will be able to input first or last name
+      ]
+    }
+  }
   const options = {
+    sort: 'createdAt',
     page: parseInt(page, 10) || 1,
     limit: parseInt(perPage, 10) || 10
   }
+
   try {
-    const docs = await User.paginate({}, options)
+    const docs = await User.paginate(query, options)
+
     res.status(200).json({ data: docs })
   } catch (e) {
     console.error(e)
