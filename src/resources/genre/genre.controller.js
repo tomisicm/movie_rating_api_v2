9032b1e2783b8615867a0fae0436e11aca069926
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-04-17 21:04:55 +0200

    genres controller

--- src/resources/genre/genre.controller.js
+++ src/resources/genre/genre.controller.js
@@ -21,7 +21,49 @@ const getAllGenres = async (req, res) => {
   }
 }
 
+export const updateGenre = async (req, res) => {
+  try {
+    const updatedDoc = await Genre.findOneAndUpdate(
+      {
+        _id: req.params.id
+      },
+      req.body,
+      { new: true }
+    )
+      .lean()
+      .exec()
+
+    if (!updatedDoc) {
+      return res.status(400).end()
+    }
+
+    res.status(200).json({ data: updatedDoc })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+export const deleteGenre = async (req, res) => {
+  try {
+    const removed = await Genre.findOneAndRemove({
+      _id: req.params.id
+    })
+
+    if (!removed) {
+      return res.status(400).end()
+    }
+
+    return res.status(200).json({ data: removed })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
 export default {
   ...crudControllers(Genre),
-  getAllGenres: getAllGenres
+  getAllGenres: getAllGenres,
+  updateOne: updateGenre,
+  removeOne: deleteGenre
 }
