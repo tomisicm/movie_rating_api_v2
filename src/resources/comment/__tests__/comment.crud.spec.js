a76eb5f81739da10b6dad1741be7594ebc75a9df
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-04-20 11:04:34 +0200

    formatting and typo ! fix tests ass soon as you finish model change !

--- src/resources/comment/__tests__/comment.crud.spec.js
+++ src/resources/comment/__tests__/comment.crud.spec.js
@@ -35,123 +35,125 @@ describe('crud controllers for comment', () => {
 
       await controllers.createOne(req, res)
     })
+  })
 
-    describe('getOne', async () => {
-      test('finds comment authenticated user and id', async () => {
-        expect.assertions(2)
+  describe('getOne', async () => {
+    test('finds comment authenticated user and id', async () => {
+      expect.assertions(2)
 
-        const user = mongoose.Types.ObjectId()
-        const item = mongoose.Types.ObjectId()
+      const user = mongoose.Types.ObjectId()
+      const item = mongoose.Types.ObjectId()
 
-        const comment = await Comment.create({
-          body: 'bodyyy',
-          item: item,
-          createdBy: user
-        })
-
-        const req = {
-          user: {
-            _id: user
-          },
-          params: {
-            id: comment._id
-          }
-        }
-
-        const res = {
-          status(status) {
-            expect(status).toBe(200)
-            return this
-          },
-          json(result) {
-            expect(result.data._id.toString()).toBe(comment._id.toString())
-          }
-        }
-
-        await controllers.getOne(req, res)
+      const comment = await Comment.create({
+        body: 'bodyyy',
+        item: item,
+        createdBy: user
       })
+
+      const req = {
+        user: {
+          _id: user
+        },
+        params: {
+          id: comment._id
+        }
+      }
+
+      const res = {
+        status(status) {
+          expect(status).toBe(200)
+          return this
+        },
+        json(result) {
+          expect(result.data._id.toString()).toBe(comment._id.toString())
+        }
+      }
+
+      await controllers.getOne(req, res)
     })
+  })
 
-    describe('getMany', async () => {
-      test('finds comments by item id', async () => {
-        expect.assertions(2)
+  describe('getMany', async () => {
+    test('finds comments by item id', async () => {
+      expect.assertions(2)
 
-        const user1 = mongoose.Types.ObjectId()
-        const user2 = mongoose.Types.ObjectId()
-        const item = mongoose.Types.ObjectId()
+      const user1 = mongoose.Types.ObjectId()
+      const user2 = mongoose.Types.ObjectId()
+      const item = mongoose.Types.ObjectId()
 
-        await Comment.create({
-          body: 'bodyyy',
-          item: item,
-          createdBy: user1
-        })
-
-        await Comment.create({
-          body: 'bodyyy',
-          item: item,
-          createdBy: user2
-        })
-
-        const req = {
-          user: {
-            _id: user1
-          },
-          params: {
-            item: item
-          },
-          query: {
-            perPage: 5,
-            page: 1
-          }
-        }
-
-        const res = {
-          status(status) {
-            expect(status).toBe(200)
-            return this
-          },
-          json(result) {
-            expect(result.data.docs).toHaveLength(2)
-          }
-        }
-
-        await controllers.getCommentsByItemId(req, res)
+      await Comment.create({
+        body: 'bodyyy',
+        item: item,
+        createdBy: user1
       })
+
+      await Comment.create({
+        body: 'bodyyy',
+        item: item,
+        itemType: ['film'],
+        createdBy: user2
+      })
+
+      const req = {
+        user: {
+          _id: user1
+        },
+        params: {
+          itemId: item
+        },
+        query: {
+          perPage: 5,
+          page: 1
+        }
+      }
+
+      const res = {
+        status(status) {
+          expect(status).toBe(200)
+          return this
+        },
+        json(result) {
+          console.log(result)
+          expect(result.data.docs).toHaveLength(2)
+        }
+      }
+
+      await controllers.getCommentsByItemId(req, res)
     })
-    describe('removeOne', async () => {
-      test('finds comment by authenticated user removes it', async () => {
-        expect.assertions(2)
+  })
+  describe('removeOne', async () => {
+    test('finds comment by authenticated user removes it', async () => {
+      expect.assertions(2)
 
-        const user = mongoose.Types.ObjectId()
-        const item = mongoose.Types.ObjectId()
+      const user = mongoose.Types.ObjectId()
+      const item = mongoose.Types.ObjectId()
 
-        const comment = await Comment.create({
-          body: 'bodyyy',
-          item: item,
-          createdBy: user
-        })
-
-        const req = {
-          user: {
-            _id: user
-          },
-          params: {
-            id: comment._id
-          }
-        }
-
-        const res = {
-          status(status) {
-            expect(status).toBe(200)
-            return this
-          },
-          json(result) {
-            expect(result.data._id.toString()).toBe(comment._id.toString())
-          }
-        }
-
-        await controllers.removeOne(req, res)
+      const comment = await Comment.create({
+        body: 'bodyyy',
+        item: item,
+        createdBy: user
       })
+
+      const req = {
+        user: {
+          _id: user
+        },
+        params: {
+          id: comment._id
+        }
+      }
+
+      const res = {
+        status(status) {
+          expect(status).toBe(200)
+          return this
+        },
+        json(result) {
+          expect(result.data._id.toString()).toBe(comment._id.toString())
+        }
+      }
+
+      await controllers.removeOne(req, res)
     })
   })
 })
